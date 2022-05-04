12908f41bab0ff87154188606faafb8a18c76e25
minato37103710 <arigatoudane@outlook.jp>
2022-05-04 10:52:50 +0900

    登録済みは弾く様にした

---
+++ functions/register.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+const User = require('../models/users');
+
+async function main() {
+    await mongoose.connect(process.env.mongo_url);
+}
+
+async function getUsers(email) {
+    if (!email) {
+        return null;
+    }else{
+        User.findOne({email:email}).then(d=>{
+            if(!d){
+                return 'unregistered';
+            }else{
+                return 'registered';
+            }
+        });
+    }
+}
+
+
+module.exports = {
+    getUsers
+}
--- routes/register.js
+++ routes/register.js
@@ -3,6 +3,7 @@ const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
 const tinyurl = require('../models/users');
+const getUsers = require('../functions/register');
 
 
 main().catch(err => console.log(err));
@@ -23,19 +24,28 @@ router.get('/', async (req, res) => {
 
 router.post('/', async (req, res) => {
     const { username, email, password } = req.body;
-    const user = new tinyurl({
-        username: username,
-        email: email,
-        password: password
+    const register = getUsers.getUsers(email);
+    register.then(async (result) => {
+        if (result === 'unregistered') {
+            const user = new tinyurl({
+                username: username,
+                email: email,
+                password: password
+            });
+            user.save().then(() => {
+                res.redirect('/');
+                console.log('user created');
+            }
+            ).catch(err => {
+                res.send(err);
+            }
+            );
+
+        } else {
+            console.log('user already registered');
+            res.render('register', { url: '', tiny: '', premiu: '', name: '', demo: '', log: 'in' });
+        }
     });
-    user.save().then(() => {
-        res.redirect('/');
-    }
-    ).catch(err => {
-        res.send(err);
-    }
-    );
-}
-);
+});
 
 module.exports = router;
