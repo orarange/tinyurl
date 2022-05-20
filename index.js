a2e850af3d6f11cef8b522ff0bbdf4332f601047
minato37103710 <81461409+minato37103710@users.noreply.github.com>
2022-05-21 08:23:34 +0900

    Update index.js

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ const rateLimit = require('express-rate-limit');
 const cron = require('node-cron');
 const cloudflare = require('cloudflare-express');
 const fs = require('fs');
+const https = require('https')
 var options = {
     key: fs.readFileSync('/etc/ssl/t-ur.site.pem'),
     cert: fs.readFileSync('/etc/ssl/t-ur.site.key'),
