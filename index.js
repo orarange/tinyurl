0b5fb61c381e95c02486078f839834826e00d638
minato37103710 <81461409+minato37103710@users.noreply.github.com>
2022-05-23 23:52:00 +0900

    Update index.js

--- index.js
+++ index.js
@@ -10,11 +10,11 @@ const rateLimit = require('express-rate-limit');
 const cron = require('node-cron');
 const cloudflare = require('cloudflare-express');
 const fs = require('fs');
-const rfs = require("rotating-file-stream").createStream("file.log", {
-  size: "10M", // rotate every 10 MegaBytes written
-  interval: "1d", // rotate daily
-  compress: "gzip" // compress rotated files
-});
+//const rfs = require("rotating-file-stream").createStream("file.log", {
+  //size: "10M", // rotate every 10 MegaBytes written
+  //interval: "1d", // rotate daily
+  //compress: "gzip" // compress rotated files
+//});
 
 const remover = require('./functions/dataremove');
 
